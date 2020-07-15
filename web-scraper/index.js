c2be056a25e2da57852a0143babd4463d7cb7245
Taylor Miller <38132620+millertn@users.noreply.github.com>
2020-07-15 13:01:58 -0400

    Web scraper to check if the keychron k1 is in stock
    - to run type "node index.js" to run

---
+++ web-scraper/index.js
@@ -0,0 +1,45 @@
+var request = require("request");
+var cheerio = require("cheerio");
+function check104Key() {
+    const options = {
+      url:
+        "https://www.keychron.com/products/keychron-k1-wireless-mechanical-keyboard?variant=31238122242137",
+      headers: {
+        "User-Agent": "request",
+      },
+    };
+    function callback(error, response, body) {
+        if(!error && response.statusCode == 200) {
+            var $ = cheerio.load(body);
+            $('.product-single__meta').filter(function() {
+                var data = $(this);
+                console.log("104 key - " + $(data).find('button').text().trim());
+            })
+        }
+    }
+    
+    request(options, callback);
+}
+
+function check87Key() {
+    const options = {
+      url:
+        "https://www.keychron.com/products/keychron-k1-wireless-mechanical-keyboard?variant=31253556625497",
+      headers: {
+        "User-Agent": "request",
+      },
+    };
+    function callback(error, response, body) {
+        if(!error && response.statusCode == 200) {
+            var $ = cheerio.load(body);
+            $('.product-single__meta').filter(function() {
+                var data = $(this);
+                console.log("87 key - " + $(data).find('button').text().trim());
+            })
+        }
+    }
+    
+    request(options, callback);
+}
+check104Key();
+check87Key();
